dee27ab9c724d9c7f64e29b46aeef0002db4384e
Mitchell Kealy <60372669+mitchellkealy@users.noreply.github.com>
2022-01-26 20:11:38 +0000

    Added statement for context

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
+# Output Q is False when exactly 0 of the 4 inputs are False (and True otherwise.)
+
 # and gate
 def and_gate(input1, input2):
-  gate1 = input1 and input2
-  return bool(gate1)
+    gate1 = input1 and input2
+    return bool(gate1)
+
 
 # not and gate
 def nand_gate(input1, input2):
-  gate1 = not(input1 and input2)
-  return bool(gate1)
+    gate1 = not (input1 and input2)
+    return bool(gate1)
+
 
 # gate for the function
 def mitchells_gate(input1, input2, input3, input4):
@@ -15,7 +19,7 @@ def mitchells_gate(input1, input2, input3, input4):
     gate3 = nand_gate(gate1, gate2)
     return bool(gate3)
 
+
 # header
 print("A\t B\t C\t D\t OUTPUT")
 print("T\t T\t T\t T\t", mitchells_gate(True, True, True, True))
-
